e851f88f9eed03af050a2867671fd64a72e67060
OwnageIsMagic <OwnageIsMagic@users.noreply.github.com>
2016-06-13 04:13:27 +0300

    some fixes

--- app.ts
+++ app.ts
@@ -4,45 +4,59 @@ import favicon = require('serve-favicon');
 import cookieParser = require('cookie-parser');
 import bodyParser = require('body-parser');
 import logger = require('morgan');
-import debug = require('debug');
 
-debug('CDDAAndroid:app.js');
+var debug = require('debug')('CDDAAndroid:app.ts');
+
+let app = express();
 
 import routes = require('./routes/index');
-var users = require('./routes/users');
-let app = express();
+import users = require('./routes/users');
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.engine('jade', require('pug').__express);
 app.set('x-powered-by', 'false');
+app.set('view engine', 'pug');
+app.engine('pug', require('pug').__express);
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use('/favicon.ico', favicon(path.join(__dirname, 'static', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+app.use('/static', express.static(path.join(__dirname, 'static'), { fallthrough: false, index: false }));
 
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function notRoutedHandler (req, res, next) {
-    var err = new Error('Not Found');
+    let err = new Error('Not Found');
     err['status'] = 404;
+    debugger; //HACK
     next(err);
     //res.render('error');
 });
 
 // error handlers
-
+app.use(function staticNotFound(err: any, req: express.Request, res: express.Response, next: express.NextFunction): void
+{
+    //process.stdout.write('\x1B[2J');
+    //debug(req._parsedUrl);
+    if (err.code === 'ENOENT' && req.url.indexOf('/static/', 0) === 0  )
+    {
+        //console.dir(err);
+        res.status(404).end();
+        return;
+    }
+    debugger; //HACK
+    next();
+    //console.log(req.baseUrl.startsWith('/static'));
+});
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function devErrorHandler (err: any, req: express.Request, res: express.Response, next: express.NextFunction): any {
+    app.use(function devErrorHandler (err: any, req: express.Request, res: express.Response, next: express.NextFunction): void {
         res.status(err.status || 500);
         res.render('error', {
             statusCode: err.status,
@@ -53,7 +67,7 @@ if (app.get('env') === 'development') {
 } else {
     // production error handler
     // no stacktraces leaked to user
-    app.use(function errorHandler (err: any, req: express.Request, res: express.Response, next: express.NextFunction): any  {
+    app.use(function errorHandler (err: any, req: express.Request, res: express.Response, next: express.NextFunction): void  {
         res.status(err.status || 500);
         res.render('error', {
             statusCode: err.status,
@@ -65,9 +79,9 @@ if (app.get('env') === 'development') {
 //NODE_ENV=production
 if (app.get('env') === 'development') {
     //import
-    var dbgPanel = require('express-debug');
+    let dbgPanel = require('express-debug');
     dbgPanel(app, { panels: ['locals', 'request', 'session', 'template', 'software_info', /*'profile',*/'other_requests', 'nav'] });
 }
 
-debug('app.js completed');
+debug('app.ts completed');
 module.exports = app;
--- routes/users.ts
+++ routes/users.ts
@@ -1,9 +1,16 @@
 ﻿import express = require('express');
+
+import routes = require('./index2');
+
 var router: express.Router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
+
+router.get('/index', routes.index);
+router.get('/contact', routes.contact);
+router.get('/about', routes.about);
 
 export = router;
